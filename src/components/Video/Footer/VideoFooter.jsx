acdcbbc2ba86ea4996db388110a358fa63746502
IMRANDIL <aliimranadil2@gmail.com>
2022-03-25 09:39:00 +0530

    yh

---
+++ src/components/Video/Footer/VideoFooter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './VideoFooter.css'
+
+
+
+
+
+
+
+
+const VideoFooter = () => {
+    return (
+        <div className='videoFooter'>
+            <h1>I am a footer</h1>
+        </div>
+    )
+}
+
+export default VideoFooter
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import VideoFooter from './Footer/VideoFooter';
 
 import './Video.css'
 
@@ -37,7 +38,7 @@ const Video = () => {
             <video className='video__player' onClick={onVideoPress} loop ref={videoRef} src="https://vod-progressive.akamaized.net/exp=1648192187~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2436%2F18%2F462183602%2F2046352575.mp4~hmac=0bbf7a6401db6943a092de8ac4e6fba156e7f2585c7bc4b3b95ee4a623a13a48/vimeo-prod-skyfire-std-us/01/2436/18/462183602/2046352575.mp4?filename=pexels-peter-billington-5454316.mp4" height='100%'></video>
 
             {/* Video Footer */}
-
+            <VideoFooter />
             {/* Video sidebar */}
         </div>
     )
