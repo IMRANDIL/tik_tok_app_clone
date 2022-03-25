af6d660d341eaa4ba93b4a501f9039a51a62d928
IMRANDIL <aliimranadil2@gmail.com>
2022-03-25 19:09:32 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Video from './components/Video/Video';
+import db from './firebase';
+
+
+
+
+
+
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+
+
+  useEffect(() => {
+
+
+    db.collection('videos').onSnapshot(snapshot => (
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    ))
+
+
+
+  }, [videos])
+
+
+
+
   return (
     <div className="app">
 
 
       <div className="app__videos">
-        <Video
+        {videos.map(({ url, channel, description, song, messages, likes, shares }) => (
+          <Video
+
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            messages={messages}
+            likes={likes}
+            shares={shares}
+
+
+
+          />
+        ))}
+
+
+
+        {/* <Video
           url="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
           channel='ImranDil'
           description='This works...!'
@@ -18,9 +63,9 @@ function App() {
           shares={235}
 
         />
+
         <Video />
-        <Video />
-        <Video />
+        <Video /> */}
       </div>
     </div>
   );
