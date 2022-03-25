9819c71dc1e0353ef933638da9c875769d8cf5b6
IMRANDIL <aliimranadil2@gmail.com>
2022-03-25 14:59:36 +0530

    yh

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -36,12 +36,12 @@ const Video = () => {
 
     return (
         <div className='video'>
-            <video className='video__player' onClick={onVideoPress} loop ref={videoRef} src="https://vod-progressive.akamaized.net/exp=1648212721~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2812%2F20%2F514062274%2F2380845364.mp4~hmac=e59d82358ab2707044b3551e09d148f6d6f199b5b656641c732cb49e27766824/vimeo-prod-skyfire-std-us/01/2812/20/514062274/2380845364.mp4?filename=pexels-cottonbro-6869572.mp4" height='100%'></video>
+            <video className='video__player' onClick={onVideoPress} loop ref={videoRef} src="https://vod-progressive.akamaized.net/exp=1648212721~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2812%2F20%2F514062274%2F2380845364.mp4~hmac=e59d82358ab2707044b3551e09d148f6d6f199b5b656641c732cb49e27766824/vimeo-prod-skyfire-std-us/01/2812/20/514062274/2380845364.mp4?filename=pexels-cottonbro-6869572.mp4" ></video>
 
             {/* Video Footer */}
             <VideoFooter channel='AliImranAdil' description='Checkout this man' song='aasha' />
             {/* Video sidebar */}
-            <VideoSidebar />
+            <VideoSidebar likes={1144} messages={652} shares={300} />
         </div>
     )
 }
--- src/components/VideoSidebar/VideoSidebar.jsx
+++ src/components/VideoSidebar/VideoSidebar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './VideoSidebar.css'
 
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MessageIcon from '@mui/icons-material/Message';
+import ShareIcon from '@mui/icons-material/Share';
 
 
 
@@ -9,12 +12,28 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 
 
+const VideoSidebar = ({ likes, shares, messages }) => {
+
+    const [like, setLike] = useState(false)
 
 
 
-const VideoSidebar = () => {
     return (
-        <div>VideoSidebar</div>
+        <div className='videoSidebar'>
+            <div className="videoSidebar__selection">
+                {like ? <FavoriteIcon fontSize='large' style={{ color: 'red' }} onClick={() => setLike(false)} /> : <FavoriteBorderIcon fontSize='large' onClick={() => setLike(true)} />}
+
+                <p>{like ? likes + 1 : likes}</p>
+            </div>
+            <div className="videoSidebar__selection">
+                <MessageIcon fontSize='large' />
+                <p>{messages}</p>
+            </div>
+            <div className="videoSidebar__selection">
+                <ShareIcon fontSize='large' />
+                <p>{shares}</p>
+            </div>
+        </div>
     )
 }
 
