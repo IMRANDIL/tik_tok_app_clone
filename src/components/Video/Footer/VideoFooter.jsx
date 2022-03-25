6448c37d5b9c9888ad94a86f772d2d2e674dbe15
IMRANDIL <aliimranadil2@gmail.com>
2022-03-25 11:11:22 +0530

    yh

--- src/components/Video/Footer/VideoFooter.jsx
+++ src/components/Video/Footer/VideoFooter.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './VideoFooter.css'
-
-
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import Ticker from 'react-ticker';
 
 
 
@@ -11,7 +12,28 @@ import './VideoFooter.css'
 const VideoFooter = () => {
     return (
         <div className='videoFooter'>
-            <h1>I am a footer</h1>
+            <div className="videoFooter__text">
+                <h3>@AliImranAdil</h3>
+                <p>This is a description</p>
+                <MusicNoteIcon />
+                <Ticker mode='smooth'>
+                    {
+                        ({ index }) => (
+                            <>
+
+                                <h1>
+                                    Yo what's up
+                                </h1>
+
+                            </>
+                        )
+
+
+
+                    }
+                </Ticker>
+            </div>
+            <img className='videoFooter__logo' src="https://static.thenounproject.com/png/934821-200.png" alt="" />
         </div>
     )
 }
